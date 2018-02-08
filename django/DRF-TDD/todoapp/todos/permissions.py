be47527b40ad1ab2a34484ff5ebcaf8b57cd79c3
Andrii Averkiiev <averkiev.jr@gmail.com>
2018-02-08 12:26:54 +0200

    remove django project

--- django/DRF-TDD/todoapp/todos/permissions.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-
-class UserIsOwnerTodo(BasePermission):
-
-    def has_object_permission(self, request, view, todo):
-        return request.user.id == todo.user.id
